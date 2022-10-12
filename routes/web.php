561ec47eec9d353295a53b7474f730acdcad50c2
GiriDiwa98 <giridiwa571@gmail.com>
2022-10-12 21:33:25 +0700

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,16 @@ Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('login',function (){
-    return view('login');
-})->name('login');
-
-Route::get('checkout',function (){
+Route::get('checkout', function () {
     return view('checkout');
 })->name('checkout');
 
-Route::get('success_checkout',function (){
+Route::get('success_checkout', function () {
     return view('success_checkout');
 })->name('success_checkout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__.'/auth.php';
